489d80f56e56a44f4921b364fcf2c4216c2d6cca
guba <gunnar.bandelow@gmail.com>
2019-03-20 10:41:16 +0100

    changed submit script to work with the given number of OMP threads

--- submit_jacobi.sh
+++ submit_jacobi.sh
@@ -1,18 +1,46 @@
 #!/bin/bash
-BASE=$(pwd)
+#SBATCH --partition=batch,atlas
+#SBATCH --time=00:10:00
+#SBATCH --nodes=1           ### Number of Nodes
+#SBATCH --ntasks-per-node=1 ### Number of tasks (MPI processes)
+#SBATCH --cpus-per-task=8  ### Number of threads per task (OMP threads)
 
-echo $BASE
-RUNFOLDER=run/
-rm -r $RUNFOLDER
-mkdir $RUNFOLDER 
+OUTPUT_IMAGES=0
+
+module purge
+module load slurm cmake/3.4.3 gcc/8.2.0 openmpi/2.1.1
+module list
+
+if [ -n "$SLURM_CPUS_PER_TASK" ]; then
+  omp_threads=$SLURM_CPUS_PER_TASK
+else
+  omp_threads=1
+fi
+
+export OMP_NUM_THREADS=$omp_threads
+
+BASE=$(pwd)
+cmake .
+make
+
+### setup run ###
+RUNFOLDER=run_$SLURM_JOB_ID/
+mkdir -p $RUNFOLDER 
 cd $RUNFOLDER
 
 cp $BASE/bin_jacobi .
 # run script
-./bin_jacobi
+srun ./bin_jacobi
 
 cd $BASE
-# plot results
+
+### plot results ###
+if [[ $OUTPUT_IMAGES ]]; then
+  #statements
+fi
+# for python3  
+module load tensorflow/1.8.0
+
 STARTTIME=$(date +%s)
 python3 plot_pcolor_out.py $RUNFOLDER
 ENDTIME=$(date +%s)
