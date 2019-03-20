36c54980cfb9e1f38de3bfb71e43a80af438eb88
guba <gunnar.bandelow@gmail.com>
2019-03-20 12:23:44 +0100

    minor changes to submit script

--- submit_jacobi.sh
+++ submit_jacobi.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-#SBATCH --partition=batch,atlas
+#SBATCH --partition=atlas
+#SBATCH --name=jacobi_example
 #SBATCH --time=00:10:00
 #SBATCH --nodes=1           ### Number of Nodes
 #SBATCH --ntasks-per-node=1 ### Number of tasks (MPI processes)
-#SBATCH --cpus-per-task=8  ### Number of threads per task (OMP threads)
+##SBATCH --cpus-per-task=1  ### Number of threads per task (OMP threads)
 
 OUTPUT_IMAGES=0
 
@@ -11,6 +12,7 @@ module purge
 module load slurm cmake/3.4.3 gcc/8.2.0 openmpi/2.1.1
 module list
 
+echo $SLURM_CPUS_PER_TASK
 if [ -n "$SLURM_CPUS_PER_TASK" ]; then
   omp_threads=$SLURM_CPUS_PER_TASK
 else
@@ -35,13 +37,12 @@ srun ./bin_jacobi
 cd $BASE
 
 ### plot results ###
-if [[ $OUTPUT_IMAGES ]]; then
-  #statements
-fi
-# for python3  
-module load tensorflow/1.8.0
+if [[ $OUTPUT_IMAGES -gt 0 ]]; then
+  # for python3  
+  module load tensorflow/1.8.0
 
-STARTTIME=$(date +%s)
-python3 plot_pcolor_out.py $RUNFOLDER
-ENDTIME=$(date +%s)
-echo "It takes $(($ENDTIME - $STARTTIME)) seconds to create all images..."
+  STARTTIME=$(date +%s)
+  python3 plot_pcolor_out.py $RUNFOLDER
+  ENDTIME=$(date +%s)
+  echo "It takes $(($ENDTIME - $STARTTIME)) seconds to create all images..."
+fi
